138cab15b30889ed73a189a1df139e2202fbe204
vannnyshka <zdanevich51@gmail.com>
2022-04-11 10:05:02 +0300

    Create games component

---
+++ gamegallery/src/app/components/games/games.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/app/models/Game';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.css']
+})
+export class GamesComponent implements OnInit {
+
+  games: Game[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.games = [
+      {
+        id: 1,
+        title: 'Fortnite',
+        price: 0.0,
+        discount: 0.0
+      },
+      {
+        id: 2,
+        title: 'Dead by Daylight',
+        price: 5.0,
+        discount: 25
+      },
+      {
+        id: 3,
+        title: 'Among us',
+        price: 1.5,
+        discount: 0.0
+      }
+    ]
+    
+  }
+
+}
---
+++ gamegallery/src/app/models/Game.ts
@@ -0,0 +1,16 @@
+export interface Game {
+    id: number;
+    title: string;
+    price: number;
+    discount: number;
+}
+
+export interface GameImage {
+    id: number;
+    title: string;
+    price: number;
+    discount: number;
+    image: 'v';
+}
+
+
